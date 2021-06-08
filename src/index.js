572c35529e437f970bbc5feb41e893ef6b952aea
Pascal Pater <pascal.pater@gmail.com>
2021-06-08 21:35:04 +0200

    setup reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from "redux";
+import allReducer from './reducers';
+
+const store = createStore(allReducer);
+
+
+// // Example of setup Redux
+// import {createStore} from 'redux';
+
+// // STORE (globalized state)
+
+// // ACTION 
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+
+// // REDUCER
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return state + 1;
+//     case DECREMENT:
+//       return state - 1;
+//   }
+// }
+
+// let store = createStore(counter);
+
+// // Display store in console
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH
+// store.dispatch(increment());
+
 
 ReactDOM.render(
     <App />, document.getElementById('root')
