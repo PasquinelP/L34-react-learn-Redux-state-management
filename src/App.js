15f74bbfa614f46cadf94cb8464ae99ee3a8c6af
Pascal Pater <pascal.pater@gmail.com>
2021-06-09 11:23:53 +0200

    use states and actions in app, create dispatch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions'
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? (
+        <h3>Valuable information only to be seen if logged in</h3>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
